59f1df56f45e40f4e3ae58f45c02ec5cb385e2d9
wuzhuo <me19@qq.com>
2018-07-25 14:20:32 +0800

    1

--- src/532.k-diff-pairs-in-an-array.go
+++ src/532.k-diff-pairs-in-an-array.go
@@ -1,1 +1,36 @@
 package main
+
+import "fmt"
+
+func findPairs(nums []int, k int) int {
+	if k < 0 {
+		return 0
+	}
+
+	count := 0
+
+	m := make(map[int]int)
+	pairs := make(map[int][]int)
+
+	for i := 0; i < len(nums); i++ {
+		m[nums[i]] = i
+	}
+
+	for i := 0; i < len(nums); i++ {
+		if v, ok := m[nums[i] + k]; ok {
+			if v != i {
+				count++
+				pairs[i] = []int{nums[i], nums[v]}
+				delete(m, nums[i] + k)
+			}
+		}
+	}
+
+	fmt.Println(m, pairs)
+
+	return count
+}
+
+func main() {
+	fmt.Println(findPairs([]int{3, 1, 4, 1, 5}, 2))
+}
